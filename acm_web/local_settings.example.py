9f26486775501e0f8dd705d866cfe2cd0ddc993f
Christopher Kotfila <kotfic@gmail.com>
2014-01-15 16:32:57 -0500

    Adding documentation for local_settings.py

---
+++ acm_web/local_settings.example.py
@@ -0,0 +1,12 @@
+import os
+
+# Please fill in the following filesystem locations and
+# Uncomment the variables then copy this file to local_settings.py
+# In the same directory.
+
+# BASEDIR should be the path to the directory that contains the
+# template/ static/ and acm_web/ directories
+# BASEDIR = "/path/to/base/directory"
+
+# Project directory
+# PROJECT_DIR = os.path.join(BASEDIR, "acm_web")
